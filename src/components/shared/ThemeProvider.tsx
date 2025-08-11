4fae3d92e28db627f95daec34088a95d9cc77866
amr-ibrahim7 <amrnew10@gmail.com>
2025-08-11 06:50:20 +0300

    refactor: Remove unused login page and clean up ThemeProvider imports

--- src/components/shared/ThemeProvider.tsx
+++ src/components/shared/ThemeProvider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { type ThemeProviderProps } from "next-themes";
 import { motion, AnimatePresence } from "framer-motion";
 import { ThemeContext } from "@/hooks/use-theme-context";
 
